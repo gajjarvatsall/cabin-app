de5c3e88e2e86e7cb5d97d8d588b0ce0664ed5e2
Vatsal Gajjar <vatsalgajjar84@gmail.com>
2023-03-21 17:30:20 +0530

    - Modified the updateCabinValue method for updating the value of UserId in the database. - Created a new method to enable user to select only one cabin at a time.

--- lib/repository/right_cabin_repository.dart
+++ lib/repository/right_cabin_repository.dart
@@ -3,10 +3,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 const String cabinRight = 'cabinRight';
 
 class CabinRepository {
-  static Future<void> updateCabinValue(String id, bool isSelected) async {
+  static Future<void> updateCabinValue(
+      String id, bool isSelected, String userId) async {
     await FirebaseFirestore.instance
         .collection(cabinRight)
         .doc(id)
-        .update({'isSelected': !isSelected});
+        .update({'isSelected': isSelected, 'userId': userId});
+  }
+
+  static Future<bool> doesNameAlreadyExist(String userId) async {
+    final QuerySnapshot result = await FirebaseFirestore.instance
+        .collection(cabinRight)
+        .where('userId', isEqualTo: userId)
+        .limit(1)
+        .get();
+    final List<DocumentSnapshot> documents = result.docs;
+    return documents.length == 1;
   }
 }
