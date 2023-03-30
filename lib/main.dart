b0f0aa922159bb834ee399cb8609606469fdca75
Vatsal Gajjar <vatsalgajjar84@gmail.com>
2023-03-30 12:48:32 +0530

    - Changed the Icon and the Name of the Application

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Cabin Cast',
       theme: ThemeData(fontFamily: 'OpenSans', useMaterial3: true),
       debugShowCheckedModeBanner: false,
       home: StreamBuilder(
