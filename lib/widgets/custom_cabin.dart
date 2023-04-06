1d727e75ea81a08594537e4a32105daeb9e7d05a
Nikunj <nikunjpanchal3333@gmail.com>
2023-04-06 18:31:27 +0530

    - Created a timer method - Marquee issue solved

--- lib/widgets/custom_cabin.dart
+++ lib/widgets/custom_cabin.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:cabin_app/utils/app_theme.dart';
 import 'package:cabin_app/widgets/custom_circle_avtar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 class CustomCabin extends StatefulWidget {
   CustomCabin({
@@ -18,62 +21,82 @@ class CustomCabin extends StatefulWidget {
 
 class _CustomCabinState extends State<CustomCabin> {
   final FirebaseAuth auth = FirebaseAuth.instance;
-  late Duration since;
+  final subjectTimer = BehaviorSubject<int>();
 
-  getTime() {
-    DateTime startTime = widget.documentSnapshot['startTime'].toDate();
-    setState(() {
-      since = DateTime.now().difference(startTime);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      setTimer();
     });
-    return since;
+  }
+
+  int sinceInSec = -1;
+
+  setTimer() {
+    if (widget.documentSnapshot['isSelected'] == true) {
+      DateTime startTime = widget.documentSnapshot['startTime'].toDate();
+      sinceInSec = DateTime.now().difference(startTime).inSeconds;
+      subjectTimer.add(sinceInSec);
+      Timer.periodic(Duration(seconds: 1), (Timer t) {
+        sinceInSec++;
+        subjectTimer.add(sinceInSec);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-    return Stack(
-      children: [
-        Container(
-          margin: const EdgeInsets.all(5),
-          width: width / 12,
-          height: width / 12,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              width: widget.documentSnapshot['isSelected'] == true ? 0 : 1,
-              color: widget.documentSnapshot['isSelected'] == true ? Colors.transparent : Colors.green,
+    return StreamBuilder<int>(
+      stream: subjectTimer,
+      builder: (context, snapshot) {
+        int timerValue = snapshot.data ?? 0;
+        return Tooltip(
+          message: widget.documentSnapshot['isSelected'] == true
+              ? "${widget.documentSnapshot['userName']}\n"
+                  "Since : "
+                  "${intToTimeLeft(timerValue)}"
+              : "",
+          child: Container(
+            margin: const EdgeInsets.all(5),
+            width: width / 12,
+            height: width / 12,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                width: widget.documentSnapshot['isSelected'] == true ? 0 : 1,
+                color: widget.documentSnapshot['isSelected'] == true ? Colors.transparent : Colors.green,
+              ),
             ),
-          ),
-          child: widget.documentSnapshot['isSelected'] == true
-              ? CustomCircleAvatar(
-                  auth: auth,
-                  imgUrl: widget.documentSnapshot['userPic'],
-                  radius: 10,
-                )
-              : Center(
-                  child: Text(
-                    "${widget.documentSnapshot['cabinName']}",
-                    style: AppTheme.titleText,
+            child: widget.documentSnapshot['isSelected'] == true
+                ? CustomCircleAvatar(
+                    auth: auth,
+                    imgUrl: widget.documentSnapshot['userPic'],
+                    radius: 10,
+                  )
+                : Center(
+                    child: Text(
+                      "${widget.documentSnapshot['cabinName']}",
+                      style: AppTheme.titleText,
+                    ),
                   ),
-                ),
-        ),
-        IconButton(
-          onPressed: () {
-            getTime();
-            showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  content: Text("Since ${since.inMinutes} Minutes"),
-                );
-              },
-            );
-          },
-          icon: Icon(Icons.info, color: widget.documentSnapshot['isSelected'] == true ? Colors.white : Colors.black),
-        ),
-      ],
+          ),
+        );
+      },
     );
+  }
+
+  String intToTimeLeft(int value) {
+    int h, m, s;
+    h = value ~/ 3600;
+    m = ((value - h * 3600)) ~/ 60;
+    s = value - (h * 3600) - (m * 60);
+    String hourLeft = h.toString().length < 2 ? "0" + h.toString() : h.toString();
+    String minuteLeft = m.toString().length < 2 ? "0" + m.toString() : m.toString();
+    String secondsLeft = s.toString().length < 2 ? "0" + s.toString() : s.toString();
+    String result = "$hourLeft:$minuteLeft:$secondsLeft";
+    return result;
   }
 }
